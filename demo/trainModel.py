68d3a137f015ad26cdde22175efde00beed0b361
Atxx737 <tsumiki201@gmail.com>
2023-06-15 12:46:16 +0700

    local - update trainModel

--- demo/trainModel.py
+++ demo/trainModel.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import datetime
-
+import keras
+import tensorflow as tf
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.losses import mean_squared_error 
 from functions_final import DeepQLearning
 
 # Get the current date and time
@@ -12,7 +15,7 @@ date_string = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")
 # TRAIN_PATH='/home/yoyoo/KLTN/detect_attack_by_reinforcement_learning/data/matrix1/normal/train-normal.csv'
 # TRAIN_PATH='../data/matrix2/normal/train.csv'
 TRAIN_PATH='../data/matrix4/normal/train.csv'
-
+MODEL_PATH = './model-15-06/trained_model_in_episode_4.h5'
 
 dataset = pd.read_csv(TRAIN_PATH)
 dataset = dataset.to_numpy()
@@ -25,12 +28,23 @@ epsilon=0.1
 
 # number of training episodes
 numberEpisodes=10
+# numberEpisodes=5
 
 stateDimension = dataset.shape[1] - 1  # column in dataset exculde label
 actionDimension = 2
 
 # create an object
 LearningQDeep = DeepQLearning(dataset,stateDimension,actionDimension,gamma,epsilon,numberEpisodes)
+
+# Load the saved model
+model = keras.models.load_model(MODEL_PATH,custom_objects={'my_loss_fn':DeepQLearning.my_loss_fn})
+
+# Assign the loaded model to the mainNetwork attribute of LearningQDeep
+LearningQDeep.mainNetwork = model
+LearningQDeep.targetNetwork = model
+
+# Compile the model with the desired optimizer, loss, and metrics
+model.compile(optimizer = RMSprop())
 
 # run the learning process
 LearningQDeep.trainingEpisodes()
@@ -43,3 +57,4 @@ LearningQDeep.mainNetwork.summary()
 # save the model, this is important, since it takes long time to train the model 
 # and we will need model in another file to visualize the trained model performance
 LearningQDeep.mainNetwork.save(f"trained_model_{date_string}.h5")
+
